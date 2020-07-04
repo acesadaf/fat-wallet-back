8e2352d69758fd54c4e2895b885964905368a87e
Sadaf Halim <acesadaf@gmail.com>
2020-07-04 18:37:19 -0500

    added expense_data functionality

--- backend/urls.py
+++ backend/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     path("add_user", views.add_user, name="add_user"),
     path("sign_in", views.sign_in, name="sign_in"),
     path("expense_submit", views.expense_submit, name="expense_submit"),
-    path("category_submit", views.category_submit, name="category_submit")
-
-
-
-
+    path("category_submit", views.category_submit, name="category_submit"),
+    path("expense_data", views.expense_data, name="expense_data")
 ]
--- backend/views.py
+++ backend/views.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse
 from backend.models import *
 import json
 import ast
+from django.http import JsonResponse
+#import decimal
 
 user_signed_in = None
 
@@ -18,6 +20,7 @@ def index(request):
     print(request.body)
     return HttpResponse("OK")
 
+
 @csrf_exempt
 def expense_submit(request):
     print("adding...")
@@ -25,7 +28,8 @@ def expense_submit(request):
     name, amount, date_of_expense, category, description, user = body["name"], body[
         "amount"], body["date_of_expense"], body["category"], body["description"], body["user"]
 
-    exp = expense(name = name, amount = amount, date = date_of_expense, category = purchase_category.objects.get(category = category), description = description, user = User.objects.get(username = user))
+    exp = expense(name=name, amount=amount, date=date_of_expense, category=purchase_category.objects.get(
+        category=category), description=description, user=User.objects.get(username=user))
     exp.save()
     print(expense.objects.all())
     return HttpResponse("Expense Added")
@@ -76,13 +80,40 @@ def sign_in(request):
 
     # Create your views here.
 
+
 @csrf_exempt
 def category_submit(request):
     print("adding...")
     body = json.loads(request.body)
     category = body["category"]
 
-    cat = purchase_category(category = category)
+    cat = purchase_category(category=category)
     cat.save()
     print(purchase_category.objects.all())
     return HttpResponse("Category Added")
+
+
+@csrf_exempt
+def expense_data(request):
+    body = json.loads(request.body)
+    requesting_user = body["username"]
+    this_user = User.objects.get(username=requesting_user)
+    expenses = expense.objects.filter(user=this_user)
+    print(expenses)
+    print("removing id and user_id...")
+    to_send = expenses.values()
+    for exp in to_send:
+        cat_id = exp['category_id']
+        category_name = purchase_category.objects.get(id=cat_id).category
+        del exp['category_id']
+        del exp['id']
+        del exp['user_id']
+        exp['category_name'] = category_name
+        exp['amount'] = float(exp['amount'])
+        exp['date'] = str(exp['date'])
+    print(to_send)
+    print("converting to json...")
+    to_send = list(to_send)
+    #post_data = json.dumps(to_send)
+    # print(post_data)
+    return JsonResponse(to_send, safe=False)
